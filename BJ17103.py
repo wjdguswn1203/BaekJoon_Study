b78c559cc03af35fff5a14f2aa8a128f450ebe95
Hyeoun_Ju <84135721+wjdguswn1203@users.noreply.github.com>
2024-07-02 15:36:20 +0900

    에라토스테네스의 체 활용으로 시간 단축

--- BJ17103.py
+++ BJ17103.py
@@ -1,5 +1,5 @@
 # 백준 17103: 골드바흐 파티션
-
+import math
 # 배열의 크기를 받아옴
 ary_num = int(input())
 ary = []
@@ -11,12 +11,18 @@ for i in range(ary_num):
 # int(math.sqrt(max(ary)+1))
 # 소수를 구하기
 prime_ary = []
-for i in range(2,max(ary)+1):
-  for j in range(2, i+1):
-    if j==i:
-      prime_ary.append(j)
-    if i%j == 0:
-      break
+n = max(ary)
+chk = [True]*(n+1)
+res = []
+chk[0], chk[1] = False, False
+for i in range(2,int(math.sqrt(n)+1)):
+  if chk[i]:
+    res.append(i)
+    j = 2
+    while i*j <= n:
+      chk[i*j] = False
+      j += 1
+prime_ary = [x for x in range(n+1) if chk[x]]
   
 # 소수끼리 더하다가, 리스트의 원소와 동일해지면 중지하고 count를 출력
 for num in ary:
