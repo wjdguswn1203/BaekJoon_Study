a8fdd5a92347942bd0d5e7c91ca218e7e5c84b45
Hyeoun_Ju <84135721+wjdguswn1203@users.noreply.github.com>
2024-08-21 09:23:28 +0900

    [Silver V] Title: 다리 놓기, Time: 56 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,13 @@
+# 백준 1010 다리놓기
+import math
+
+def factorial(N):
+    if N > 1:
+        return (N * factorial(N-1))
+    else:
+        return 1
+
+T = int(input())
+for _ in range(T):
+    N, M = list(map(int, input().split()))
+    print (factorial(M) //  (factorial(M-N) * factorial(N)))
